834ca0f772d2f929f7509040e54b1cbd0d2f0e1e
c0ffee0wl <c0ffee0wl@gmx.net>
2025-05-13 03:29:30 -0400

    Changed docker to CE and added ad-recon in installer

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 tools_dir="/opt/BA_tools"
 sudo mkdir "${tools_dir}" 2>/dev/null
 sudo chown -R "$(whoami)":"$(whoami)" "${tools_dir}"
-sudo apt install docker.io
+sudo apt install -y docker-ce
 
 python3 -m venv "${tools_dir}/.venv"
 source "${tools_dir}/.venv/bin/activate"
-pip3 install py2neo pandas prettytable neo4j tabulate argcomplete alive-progress "numpy<1.29.0" colorama requests --upgrade
+pip3 install py2neo pandas prettytable neo4j tabulate argcomplete alive-progress "numpy<1.29.0" colorama requests termcolor --upgrade
 deactivate
 pipx install git+https://github.com/dirkjanm/bloodhound.py --force
 pipx install "git+https://github.com/dirkjanm/BloodHound.py@bloodhound-ce" --force --suffix '_ce'
@@ -19,3 +19,5 @@ unzip -o "${tools_dir}"/PlumHound.zip -d "${tools_dir}"
 wget https://raw.githubusercontent.com/zeronetworks/BloodHound-Tools/main/Ransomulator/ransomulator.py -O "${tools_dir}"/ransomulator.py
 wget https://raw.githubusercontent.com/kaluche/bloodhound-quickwin/main/bhqc.py -O "${tools_dir}"/bhqc.py
 pipx install git+https://github.com/idnahacks/GoodHound --force
+
+git clone https://github.com/tid35/ad-recon "${tools_dir}"/ad-recon
